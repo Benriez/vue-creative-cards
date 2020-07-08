6baf3a6149fab193016cfabb823bf397bae8fda8
Benriez <33022072+Benriez@users.noreply.github.com>
2020-07-08 14:18:51 +0200

    removed text component

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.component('text-input', {
-  template: '<textarea :placeholder="textValue" @click="changeText"></textarea>',
-  data: function(){
-    return {
-      textValue: 'Type here'
-    }
-  },
-  methods: {
-    changeText: function(){
-      this.textValue = 'Text changed'
-    }
-  }
-})
+
 
 new Vue({
   el: '#app',
