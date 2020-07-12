c885968b15fdb20fd8179de4403c97f1c887aa7a
Benriez <33022072+Benriez@users.noreply.github.com>
2020-07-12 17:45:38 +0200

    added div class "edit-area" and "edit-display"

---
+++ src/components/card/CardFront.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <div class="col-sm-6 card edit-area">
+
+        </div> 
+        <div class="col-sm-6 card edit-display">
+
+        </div> 
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
