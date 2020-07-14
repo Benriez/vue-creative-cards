0c0dabe8ee751b9c9511c168b46c5f98999f870d
Benriez <33022072+Benriez@users.noreply.github.com>
2020-07-14 20:43:19 +0200

    created ImageOutput component

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -9,7 +9,7 @@
         </div> 
         <div class="col-sm-6 card card-display">
             <cc-text-output :displayText="textBoxValue1" :containerHeight="130"></cc-text-output>
-            <cc-image-output :displayImage="imageName" :containerHeight="350"></cc-image-output>
+            <cc-image-output :displayImage="imageName" :containerHeight="130"></cc-image-output>
             <cc-text-output :displayText="textBoxValue2" :containerHeight="130"></cc-text-output>
             <cc-text-output :displayText="textBoxValue3" :containerHeight="130"></cc-text-output>	
         </div> 
