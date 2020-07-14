51045426d13ad076f580b8c91831c3feaddb6e4c
Benriez <33022072+Benriez@users.noreply.github.com>
2020-07-14 18:52:45 +0200

    add firebaseConfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCr0Cu2Chms5RqM3V5KIgzaDZD2mD6OCjI",
+    authDomain: "creative-cards-app-5e18d.firebaseapp.com",
+    databaseURL: "https://creative-cards-app-5e18d.firebaseio.com",
+    projectId: "creative-cards-app-5e18d",
+    storageBucket: "creative-cards-app-5e18d.appspot.com",
+    messagingSenderId: "287152301767",
+    appId: "1:287152301767:web:402087d19f3dedbd3200e2",
+    measurementId: "G-E6KYW7GYF8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
